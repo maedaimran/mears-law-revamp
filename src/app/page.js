aa9930d4acfb427501e4b33e0dd10a0058718090
maedaimran <145614109+maedaimran@users.noreply.github.com>
2025-10-09 12:27:37 -0400

    Updated merge with hero and header/footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <>
+    <div className="page">
       <Header />
+      <main>
+        <Hero />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }

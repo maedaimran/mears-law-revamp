5c4f88ba37bbf2526eeece45ea8faf0a2ae5483d
maedaimran <145614109+maedaimran@users.noreply.github.com>
2025-10-30 12:36:08 -0400

    Talk to lawyer section on home

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Containers from "./components/containers.js";
 import OurValues from "./components/OurValues.js";
+import CallToAction from "./components/CallToAction.js";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hero from "./components/hero";
@@ -18,6 +19,9 @@ export default function Home() {
         <section>
           <Containers />
         </section>
+        <section>
+        <CallToAction />
+      </section>
       {/* </main> */}
     </div>
   );
